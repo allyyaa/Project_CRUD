6b6bd28655979a04abb56e4c7fa27639f31d0cdb
allyyaa <alyadewiintanr@gmail.com>
2023-12-18 14:03:15 +0700

    Merubah File index.php

--- tambahdata.php
+++ tambahdata.php
@@ -25,7 +25,7 @@ if(isset($_POST['submit'])){
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tambah Data</title>
+    <title>Tambah Data Mahasiswa</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
